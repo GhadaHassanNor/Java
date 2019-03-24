e02ba406c046c207ed93e2969963766dc8817b7f
Ghada Hassan <Kareena.Kapour13@gmail.com>
2019-03-24 04:51:04 +0200

    initial version

---
+++ src/buzzle/Buzzle.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buzzle;
+
+/**
+ *
+ * @author MAGIC
+ */
+public class Buzzle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int a,b=1,c,d,e,f,g;
+        for(a=1;a<=20;a+=2){
+            for(e=1;e<=a;e+=2){
+                if(a%e!=0){
+                    System.out.println(b+"=>"+a);
+                    b++;
+                    break;
+                }
+            }
+            //System.out.println(b+"=>"+a);
+            //b++;
+        }
+        for(c=1;c<=20;c+=3){
+            for(f=1;f<=c;f+=3){
+                if(c%f!=0){
+                    System.out.println(b+"=>"+c);
+                    b++;
+                    break;
+                }
+            }
+            //System.out.println(b+"=>"+c);
+            //b++;
+        }
+        for(d=1;d<=20;d+=4){
+            for(g=1;g<=d;g+=4){
+                if(d%g!=0){
+                    System.out.println(b+"=>"+d);
+                    b++;
+                    break;
+                }
+            }
+            //System.out.println(b+"=>"+d);
+            //b++;
+        }*/
+        
+        
+    }
+    
+}
